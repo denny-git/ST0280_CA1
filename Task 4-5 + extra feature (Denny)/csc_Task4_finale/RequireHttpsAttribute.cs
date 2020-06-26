843e3be895adf36c27cc8fe21c3fdd93aa0205d4
denny-git <56954790+denny-git@users.noreply.github.com>
2020-06-26 18:21:49 +0800

    small change to Task 4

--- Task 4-5 + extra feature (Denny)/csc_Task4_finale/RequireHttpsAttribute.cs
+++ Task 4-5 + extra feature (Denny)/csc_Task4_finale/RequireHttpsAttribute.cs
@@ -17,12 +17,6 @@ namespace EmployeeService
                     .CreateResponse(HttpStatusCode.Found);
                 actionContext.Response.Content = new StringContent
                     ("<p>Use https instead of http</p>", Encoding.UTF8, "text/html");
-
-                UriBuilder uriBuilder = new UriBuilder(actionContext.Request.RequestUri);
-                uriBuilder.Scheme = Uri.UriSchemeHttps;
-                uriBuilder.Port = 44311;
-
-                actionContext.Response.Headers.Location = uriBuilder.Uri;
             }
             else
             {
